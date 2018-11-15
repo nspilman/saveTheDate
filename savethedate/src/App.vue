3689737ad9e392fbad6b6b6609d97630f176e92f
Nate Spilman <Nate.Spilman@gmail.com>
2018-11-14 18:02:13 -0600

    making site

--- savethedate/src/App.vue
+++ savethedate/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class = 'row'>
+      <div class = 'col-sm-7'>
+      </div>
+      <div class = 'col-sm-5'>
+        <login></login>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import login from "./components/login.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    login
   }
 };
 </script>
@@ -23,6 +28,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height:100vh;
+  background-image:url('assets/background.png');
+  background-size: 100% auto;
 }
+@import "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";
+
 </style>
---
+++ savethedate/src/components/login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class ='main text-center'>
+        <h1>Save the Date</h1>
+        </div>
+</template>
+
+
+<script>
+export default {
+  name: "login",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style>
+h1 {
+    color:white !important;
+}
+
+.main{
+    display:flex !important;
+    justify-content: center;
+    height:100vh;
+    flex-direction: column;
+    align-content: center;
+    font-size:100em;
+}
+
+</style>
